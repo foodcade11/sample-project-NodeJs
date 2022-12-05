45660595d1095eef6fc051e53bf26fb2d20a1ddf
foodcade <maancadde19@gmail.com>
2022-12-05 01:18:26 -0800

    initial commit

---
+++ app.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var app=express();
+
+app.set('view engine','ejs');
+
+
+
+
+
+
+
+app.listen(3000);
+
+app.get('/',function(req,res){
+    res.render('home' );
+    // console.log(req.query);
+});
+app.get('/adduser',function(req,res){
+    res.render('adduser' );
+    // console.log(req.query);
+});
